696c4049817e7bd353488fb21865e7e9c3d382eb
lekaf <tony.seperoumal@gmail.com>
2015-07-23 17:30:02 +0200

    create and test

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+
+	include('db.php');
+
+	$sql = "SELECT Name 
+			FROM country
+			WHERE Name LIKE :kw";
+
+	$sth = $dbh->prepare($sql);
+	$sth->bindValue(':kw', $_GET['kw'] . '%');
+	$sth->execute();
+
+	$countries = $sth->fetchAll();
+
+	foreach($countries as $country){
+		echo $country['Name'] . '<br />';
+	}
